1e6d5b0fa85a387d86edcb975171d9ae10549354
SBI- <pascal.huerlimann@gmail.com>
2017-10-06 11:09:20 +0200

    Remove user prompts

--- clean.ps1
+++ clean.ps1
@@ -2,5 +2,3 @@ rm -r .\workspace -ea ig
 rm -r .\conf\jetty\gen\org* -ea ig
 rm -r .\conf\jetty\gen\com* -ea ig
 rm derby.log -ea ig
-
-Read-Host -Prompt "Clean Successful! Press Enter to close this window"
