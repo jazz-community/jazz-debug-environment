3b80960215d89cfa103113f34ba4658f06646e13
SBI- <pascal.huerlimann@gmail.com>
2017-10-19 15:44:19 +0200

    Fix launch path

---
+++ scripts/click_launch.ps1
@@ -0,0 +1,6 @@
+echo "Starting the Jazz Debug Environment... (this will likely take a couple of minutes, depending on your machine)"
+echo "Navigate to 'https://localhost:7443/jazz' to access the application"
+
+& ((Split-Path $MyInvocation.InvocationName) + "\scripts\run_jetty.ps1")
+
+Read-Host -Prompt "For some reason, the jazz debug session did not start properly. Carefully read the entries above (if there are any) and consult the logs. Press Enter to Close this Window"
