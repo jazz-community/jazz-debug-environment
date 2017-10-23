a4a0306dc43674428655f63a4314059d5004e4a4
SBI- <pascal.huerlimann@gmail.com>
2017-10-23 12:35:54 +0200

    Add more cleanup options and complete hard-coded linux build

---
+++ scripts/kill.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf db/conf
+rm -rf sdk/features sdk/license sdk/plugins
+rm -rf jre/bin jre/docs jre/lib jre/plugin jre/properties
+rm -f jre/copyright jre/notices.txt jre/readme.txt
