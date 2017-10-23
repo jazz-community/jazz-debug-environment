4f82309ad62ab6f76c84d73800b0b8910853438b
SBI- <pascal.huerlimann@gmail.com>
2017-10-23 13:43:25 +0200

    Dynamically resolve sdk version

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -2,5 +2,6 @@
 
 rm -rf db/conf
 rm -rf sdk/features sdk/license sdk/plugins
+rm -f sdk/artifacts.jar sdk/content.jar
 rm -rf jre/bin jre/docs jre/lib jre/plugin jre/properties
 rm -f jre/copyright jre/notices.txt jre/readme.txt
