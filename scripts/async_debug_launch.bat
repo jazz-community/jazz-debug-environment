5203e30a3295f3e73e734511911b7b52c87696fc
SBI- <pascal.huerlimann@gmail.com>
2018-10-14 19:53:33 +0200

    Prepare for migration

--- scripts/async_debug_launch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@start powershell.exe -file %1
-
-:: Ugly hack has to be used instead of the built-in timeout command because
-:: IntelliJ, for reasons, doesn't allow the timeout command in external 
-:: tools.
-
-ping 127.0.0.1 -n 10 > NUL
--- scripts/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf workspace/
-rm -rf conf/jetty/gen/org*
-rm -rf conf/jetty/gen/com*
-rm -f derby.log
